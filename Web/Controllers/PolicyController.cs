92820176b1d2d27afacf31bd5d57ef66a675af4f
James Nathan Belue <computerghost@hotmail.com>
2021-05-01 13:16:09 -0500

    Added in some legal documents

---
+++ Web/Controllers/PolicyController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Controllers
+{
+    public class PolicyController : Controller
+    {
+        public IActionResult ContentPolicy()
+        {
+            return View();
+        }
+
+        public IActionResult CookiePolicy()
+        {
+            return View();
+        }
+
+        public IActionResult FairUseNotice()
+        {
+            return View();
+        }
+
+        public IActionResult PrivacyPolicy()
+        {
+            return View();
+        }
+    }
+}
