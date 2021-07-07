dad54a3ae903645c449a4b801bce173cc4199f86
James Nathan Belue <computerghost@hotmail.com>
2021-07-06 23:50:16 -0500

    Removed unneeded pages

--- CMS/ClientApp/src/components/NavDrawer.tsx
+++ CMS/ClientApp/src/components/NavDrawer.tsx
@@ -64,7 +64,6 @@ export default function NavDrawer({ width, open, allowClose, onDrawerClosed }: I
             <Typography variant='h6' className={classes.header}>Content</Typography>
             <List>
                 <DrawerItem text='Articles' icon={faPenNib} href='/articles' />
-                <DrawerItem text='Leads' icon={faLightbulb} href='/' />
                 <DrawerItem text='Comments' icon={faComments} href='/comments' />
                 <DrawerItem text='Media' icon={faPhotoVideo} href='/media' />
             </List>
@@ -79,7 +78,6 @@ export default function NavDrawer({ width, open, allowClose, onDrawerClosed }: I
 
             <Typography variant='h6' className={classes.header}>Information</Typography>
             <List>
-                <DrawerItem text='Licenses' icon={faHandshake} href='/' />
                 <DrawerItem text='Help and Tips' icon={faQuestionCircle} href='/' />
             </List>
 
