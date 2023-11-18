fe69ce9c53d96d745f6c2770b15129ecff225f16
James Nathan Belue <computerghost@hotmail.com>
2023-11-18 13:34:28 -0800

    Placeholder for publicweb api

---
+++ Src/Presentation/PublicWeb/Controllers/ApiController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PublicWeb.Controllers;
+
+public class ApiController : Controller
+{
+    // This controller will later handle the ajax calls from the publicweb frontend.
+}
