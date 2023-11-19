d60b97194c9cfc256973bc12ad21464f7bd56f1e
James Nathan Belue <computerghost@hotmail.com>
2023-11-19 12:39:01 -0800

    Added API project

---
+++ Src/Presentation/API/Controllers/ArticlesController.cs
@@ -0,0 +1,25 @@
+﻿using Core.Modules.ArticleList.Models;
+using Core.Modules.ArticleList.Queries.GetMostRecent;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ArticlesController : ControllerBase
+{
+    private readonly ISender _sender;
+
+    public ArticlesController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    [HttpGet]
+    public Task<PagedArticles> Index()
+    {
+        var query = new GetMostRecentArticlesQuery();
+        return _sender.Send(query);
+    }
+}
